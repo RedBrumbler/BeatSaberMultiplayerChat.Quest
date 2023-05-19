9ac38e5356851ec3df5fdf446149b34b912c8b68
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-05-19 15:47:24 +0200

    Actually install things

--- src/Installers/MenuInstaller.cpp
+++ src/Installers/MenuInstaller.cpp
@@ -1,10 +1,38 @@
 #include "Installers/MenuInstaller.hpp"
 
+#include "lapiz/shared/utilities/ZenjectExtensions.hpp"
+#include "Zenject/DiContainer.hpp"
+#include "Zenject/FromBinderNonGeneric.hpp"
+#include "Zenject/ConcreteIdBinderGeneric_1.hpp"
+
+#include "Core/LobbyIntegrator.hpp"
+#include "UI/Hud/HudVoiceIndicator.hpp"
+
+#include "UI/Lobby/ChatViewController.hpp"
+#include "UI/ModSettings/ModSettingsViewController.hpp"
+#include "UI/ModSettings/ModSettingsFlowCoordinator.hpp"
+#include "UI/ModSettings/ModSettingsMenuController.hpp"
+
 DEFINE_TYPE(MultiplayerChat::Installers, MenuInstaller);
+
+using namespace Lapiz::Zenject::ZenjectExtensions;
 
 namespace MultiplayerChat::Installers {
     void MenuInstaller::InstallBindings() {
         auto container = get_Container();
-        // TODO
+
+        // UI - Menu
+        FromNewComponentAsViewController(container->Bind<UI::ModSettings::ModSettingsViewController*>())->AsSingle();
+        FromNewComponentOnNewGameObject(container->Bind<UI::ModSettings::ModSettingsFlowCoordinator*>())->AsSingle();
+        container->BindInterfacesAndSelfTo<UI::ModSettings::ModSettingsMenuController*>()->AsSingle();
+
+        // UI - Lobby
+        FromNewComponentAsViewController(container->Bind<UI::Lobby::ChatViewController*>())->AsSingle();
+
+        // Core
+        container->BindInterfacesAndSelfTo<Core::LobbyIntegrator*>()->AsSingle();
+
+        // HUD
+        FromNewComponentOnNewGameObject(container->BindInterfacesAndSelfTo<UI::Hud::HudVoiceIndicator*>())->AsSingle();
     }
 }
--- src/Installers/MultiplayerInstaller.cpp
+++ src/Installers/MultiplayerInstaller.cpp
@@ -1,10 +1,20 @@
 #include "Installers/MultiplayerInstaller.hpp"
 
+#include "lapiz/shared/utilities/ZenjectExtensions.hpp"
+#include "Zenject/DiContainer.hpp"
+#include "Zenject/FromBinderNonGeneric.hpp"
+
+#include "Core/GameplayIntegrator.hpp"
+#include "UI/Hud/HudVoiceIndicator.hpp"
+
 DEFINE_TYPE(MultiplayerChat::Installers, MultiplayerInstaller);
 
+using namespace Lapiz::Zenject::ZenjectExtensions;
 namespace MultiplayerChat::Installers {
     void MultiplayerInstaller::InstallBindings() {
         auto container = get_Container();
-        // TODO
+
+        container->BindInterfacesAndSelfTo<Core::GameplayIntegrator*>()->AsSingle();
+        FromNewComponentOnNewGameObject(container->BindInterfacesAndSelfTo<UI::Hud::HudVoiceIndicator*>())->AsSingle();
     }
 }
