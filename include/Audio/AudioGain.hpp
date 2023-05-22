e37babfbb76bdf0a49b025d0af9017e54791f9b9
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-05-22 16:10:42 +0200

    if gain is 1 don't apply it

--- include/Audio/AudioGain.hpp
+++ include/Audio/AudioGain.hpp
@@ -6,12 +6,15 @@
 namespace MultiplayerChat::Audio {
     struct AudioGain {
         static inline void Apply(float* begin, float* end, float gain) {
+            if ((gain - 1.0f) < 0.001f) return;
             for (auto itr = begin; itr < end; itr++) *itr = *itr * gain;
         }
         static inline void Apply(ArrayW<float> samples, float gain) {
+            if ((gain - 1.0f) < 0.001f) return;
             for (auto& sample : samples) sample *= gain;
         }
         static inline void Apply(std::vector<float> samples, float gain) {
+            if ((gain - 1.0f) < 0.001f) return;
             for (auto& sample : samples) sample *= gain;
         }
     };
